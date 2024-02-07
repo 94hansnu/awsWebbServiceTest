3c9ffd0d7f38f1b215706d322fb699e2d7e39243
94hansnu <94hansnu@gafe.molndal.se>
2024-02-07 11:21:19 +0100

    Initial commit

---
+++ src/main/java/com/example/awsWebbServiceTest/AwsWebbServiceTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsWebbServiceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWebbServiceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsWebbServiceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsWebbServiceTest/AwsWebbServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsWebbServiceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsWebbServiceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
